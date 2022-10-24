72b394c4f0d61ac0051ac13f63442914ef823195
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-10-24 06:15:24 +0600

    home page design partially completed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import './Home.css';
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='home-bg'>
+            <div className='bg-overlay'>
+                <div className="grid grid-cols-3">
+                    <div className='text-white my-52 px-16'>
+                        <h1 className='text-7xl font-bold'>COX'S BAZAR</h1>
+                        <p className='text-2xl'>Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
+                        <button className='btn btn-warning mt-16'>Booking</button>
+                    </div>
+
+                    <div className='col-span-2'>
+                        <h1>fasdfawetwef</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../Assets/logo1.png';
 
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navigation</h1>
-        </div>
+        <header className="p-2 bg-amber-400 text-gray-100">
+            <div className="container flex justify-between h-16 mx-auto">
+                <div className="flex">
+                    <Link to="/" aria-label="Back to homepage" className="flex items-center p-2">
+                        <img src={logo} alt="" />
+                    </Link>
+                    <ul className="items-stretch hidden space-x-3 lg:flex">
+                        <li className="flex">
+                            <Link to="/news" className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-xl font-semibold">News</Link>
+                        </li>
+                        <li className="flex">
+                            <Link to="/destination" className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-xl font-semibold">Destination</Link>
+                        </li>
+                        <li className="flex">
+                            <Link to="/blog" className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-xl font-semibold">Blog</Link>
+                        </li>
+                        <li className="flex">
+                            <Link to="/contact" className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-xl font-semibold">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="items-center flex-shrink-0 hidden lg:flex">
+                    <div className="flex items-center md:space-x-4">
+                        <button type="button" className="px-8 py-3 font-semibold rounded bg-indigo-600 text-white mr-3">Log in</button>
+                    </div>
+                    <button className="px-8 py-3 font-semibold rounded bg-sky-600 text-white">Register</button>
+                </div>
+                <button className="p-4 lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 text-gray-100">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                </button>
+            </div>
+        </header>
     );
 };
 
